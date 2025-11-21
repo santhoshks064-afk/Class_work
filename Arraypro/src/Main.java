20b0197c218eee04bcd3da167a04ef457898368f
santhoshks064-afk <santhoshks064@gmail.com>
2025-11-21 18:17:19 +0530

    hello world

---
+++ Arraypro/src/Main.java
@@ -0,0 +1,84 @@
+
+import java.util.Scanner;
+
+
+public class Main {
+
+   
+    public static void main(String[] args) {
+     /* Scanner sr= new Scanner(System.in);
+        System.out.println("welcome");
+        System.out.println("ENTER THE ARRAY LENGTH ENTER THE VALVE ");
+                int size= sr.nextInt();
+                int ar[]=new int[size];
+                int sum=0;
+               int choose;
+                for(int i=0;i<ar.length;i++){
+                  ar[i]=sr.nextInt();
+                }
+              do{   
+                System.out.println("1 array max");
+                System.out.println("2 array min");
+                System.out.println("3 reverse");
+                System.out.println("4 even add");
+                System.out.println("5 sum");
+                int option=sr.nextInt();
+             
+                  if(option==1)
+               {int max=ar[0];
+                    for(int i=0;i<ar.length;i++){
+                         if(ar[i]>max)
+                         {
+                            max=ar[i];
+                         }
+                    }System.out.println(max);
+                } 
+              else if(option==2){
+                  int min=ar[0];
+                  for(int i=0;i<ar.length;i++){
+                     if(ar[i]<min) 
+                     {
+                         min=ar[i];
+                     }
+                  }System.out.println(min);
+              }
+              else if(option==3){
+                  for(int i=ar.length-1;i>=0;i--){
+                      System.out.println(ar[i]);   
+                  }
+              }  
+              else if(option==4){
+               
+                  for(int i=0;i<ar.length;i++){
+                  if(ar[i]%2==0){
+                   
+                       System.out.println("even");
+                  }else{
+                      System.out.println("add");
+              }
+    }
+              }
+              else if(option==5){
+                  for(int i=0;i<ar.length;i++){
+                      sum=sum+ar[i];
+                      
+                  }
+                  System.out.println(sum);
+              }
+              else{
+                  System.out.println("invaild");
+                 
+              }   System.out.println("1 continuous");
+                  System.out.println("2 exit");
+                choose=sr.nextInt();
+         
+    }while(choose==1);*/
+     /*   int i=0;
+     while(i<10) { 
+       i++;
+         System.out.println(i);
+     }
+              System.out.println(i); */
+    }
+}
+
